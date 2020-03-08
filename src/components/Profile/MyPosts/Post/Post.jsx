a67616e316dd6e60650cc540895c6757d60728b3
CrazyDog1811 <evgeshatertyshnyi@mail.ru>
2020-03-08 13:59:02 +0300

    SoSo

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import classes from'./Post.module.css';
-const Post = () => {
+const Post = (props) => {
     return  (  
      <div className={classes.item}>
          <img src='https://cs.pikabu.ru/post_img/big/2014/01/01/2/1388676982_651577815.jpg' alt='kitty' width='50px' height='50px'></img>
-     post1 
+     {props.messagegit add .} 
      <span>like</span>
     </div>
     )
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import classes from'./Profile.module.css';
 import MyPosts from'./MyPosts/MyPosts.jsx';
 import Post from './MyPosts/Post/Post';
-const Profile = () => {
+const Profile = (props) => {
     return   <main className={classes.profile}>
     <img src="https://www.bing.com/th?id=OIP.AGTOByuvPlmrByuwYXm72QHaDs&pid=Api&rs=1" alt="space" width="100%" height="100px"></img>
     <div className={classes.avatar}>
       <img src="http://i.huffpost.com/gen/781019/images/o-DMX-COMPUTER-RAPPER-GOOGLE-VIDEO-facebook.jpg" alt="I am black" width="80" height="80"></img>
     </div>
    <MyPosts />
-   <Post />
-   <Post />
-   <Post />
-   <Post />
-   <Post />
+   <Post message="Hi, how are you?"/>
+   <Post message="Wow, hwo's there? "/>
+   <Post message="Yo, man"/>
+   <Post message="What's a f...."/>
+   <Post message="????"/>
   </main>
 }
 
