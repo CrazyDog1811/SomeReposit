1e6fe0307eda9c2cdf0771b861492f4217a79559
CrazyDog1811 <evgeshatertyshnyi@mail.ru>
2020-03-31 17:10:21 +0300

    DAL

---
+++ src/api/api.js
@@ -0,0 +1,18 @@
+import * as axios from 'axios';
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: "https://social-network.samuraijs.com/api/1.0/",
+    headers: {
+        "API-KEY": "bfec528d-df0b-4f80-9e1e-afa4fb5f8eed"
+    }
+});
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+    return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+      .then(response => {
+        return   response.data; 
+      });
+}
+}
