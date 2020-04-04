d5319a56d0b41133437944f454d21dfcaad63e3a
CrazyDog1811 <evgeshatertyshnyi@mail.ru>
2020-04-04 15:10:17 +0300

    hoc

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,6 +3,7 @@ import Profile from './Profile';
 import { connect } from 'react-redux';
 import {getUserProfile} from '../../redux/profile-reducer';
 import { withRouter, Redirect } from 'react-router-dom';
+import { withAuthRedirect } from '../../hoc/withAuthRedirect';
 
 
 
@@ -19,19 +20,19 @@ class ProfileContainer extends React.Component {
     //     }) 
   }
 
-  render() {
-    if(!this.props.isAuth) return <Redirect to={"/login"} />;
+  render() { 
   return <div>
    <Profile {...this.props} profile={this.props.profile}/>
   </div>
   } 
 }
 
+let AuthRedirectComponent = withAuthRedirect(ProfileContainer);
+
 let mapStateToProps = (State) => ({
-    profile: State.profilePage.profile,
-    isAuth: State.auth.isAuth,
+  profile: State.profilePage.profile,
 });
 
-let WithUrlDataContainerComponent = withRouter(ProfileContainer);
+let WithUrlDataContainerComponent = withRouter(AuthRedirectComponent);
 
 export default connect(mapStateToProps, {getUserProfile}) (WithUrlDataContainerComponent);
