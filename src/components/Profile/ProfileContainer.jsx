143f375e49e243fc55a926622468d4d34be153ff
CrazyDog1811 <evgeshatertyshnyi@mail.ru>
2020-04-06 18:18:40 +0300

    compose

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Profile from './Profile';
 import { connect } from 'react-redux';
-import {getUserProfile} from '../../redux/profile-reducer';
+import { getUserProfile } from '../../redux/profile-reducer';
 import { withRouter, Redirect } from 'react-router-dom';
 import { withAuthRedirect } from '../../hoc/withAuthRedirect';
+import { compose } from 'redux';
 
 
 
@@ -11,28 +12,25 @@ class ProfileContainer extends React.Component {
 
   componentDidMount() {
     let userId = this.props.match.params.userId;
-    if(!userId) {
+    if (!userId) {
       userId = 6596;
     }
     this.props.getUserProfile(userId);
-    // usersAPI.getProfile(userId).then(response => {
-    //         this.props.setUserProfile(response.data);
-    //     }) 
   }
 
-  render() { 
-  return <div>
-   <Profile {...this.props} profile={this.props.profile}/>
-  </div>
-  } 
+  render() {
+    return <div>
+      <Profile {...this.props} profile={this.props.profile} />
+    </div>
+  }
 }
-
-let AuthRedirectComponent = withAuthRedirect(ProfileContainer);
 
 let mapStateToProps = (State) => ({
   profile: State.profilePage.profile,
 });
 
-let WithUrlDataContainerComponent = withRouter(AuthRedirectComponent);
-
-export default connect(mapStateToProps, {getUserProfile}) (WithUrlDataContainerComponent);
+export default compose(
+  connect(mapStateToProps, { getUserProfile }),
+  withRouter,
+  withAuthRedirect
+)(ProfileContainer);
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,34 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { follow, unfollow,  setCurrentPage, toggleFollowingProgress, getUsers } from '../../redux/users-reducer';
-import * as axios from 'axios';
 import Users from './Users';
 import Preloader from '../common/preloader/preloader';
-import { usersAPI } from '../../api/api';
+import { compose } from 'redux';
 
 class UsersContainer extends React.Component {
 
     componentDidMount() {
         this.props.getUsers(this.props.currentPage, this.props.pageSize);
-        /* this.props.toggleIsFetching(true);
-
-        usersAPI.getUsers(this.props.currentPage, this.props.pageSize)
-        .then(data => {
-            this.props.toggleIsFetching(false);
-            this.props.setUsers(data.items);
-            this.props.setUsersTotalCount(data.totalCount);
-        }) */
     };
 
     onPageChanged = (pageNumber) => {
         this.props.getUsers(pageNumber, this.props.pageSize);
-       /* this.props.setCurrentPage(pageNumber);
-        this.props.toggleIsFetching(true);
-        usersAPI.getUsers(pageNumber, this.props.pageSize)
-        .then( data => {
-            this.props.toggleIsFetching(false);
-            this.props.setUsers(data.items);
-        }) */
     };
 
     render() {
@@ -59,6 +43,7 @@ let mapStateToProps = (State) => {
     }
 };
 
-export default connect(mapStateToProps,{ follow, unfollow, setCurrentPage, toggleFollowingProgress, getUsers })(UsersContainer);
+
+export default compose(connect(mapStateToProps,{ follow, unfollow, setCurrentPage, toggleFollowingProgress, getUsers })(UsersContainer));
 
 
